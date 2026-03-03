be3e68751b972fd5249bda6540d32fc32a619086
Joe Leary <jleary@digitalreign.us>
2026-03-03 16:33:39 -0500

    Workflow troubleshooting

--- dr-helpdesk-webapp/obj/Debug/net8.0/dr-helpdesk-webapp.AssemblyInfo.cs
+++ dr-helpdesk-webapp/obj/Debug/net8.0/dr-helpdesk-webapp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dr-helpdesk-webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26d58080fd4abd942476698f830114d554b4486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3e663aee5b7a5d3a74daadc9a793a7b80d14e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("dr-helpdesk-webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dr-helpdesk-webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
